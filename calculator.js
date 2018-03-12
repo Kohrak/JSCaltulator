2a8ab675e62aeb42d54e1f164bb9c974f8a4e8df
Kohrak <Gabriel@PPM.local>
2018-03-12 14:53:13 +0000

    basic calculator functionality added, can also chain operations

--- calculator.js
+++ calculator.js
@@ -1,2 +1,22 @@
-alert("connected");
 //take input and pass it to math.eval()
+var numbers = "";
+var buttons = document.querySelectorAll("button")
+
+for(var i = 0; i < buttons.length - 2; i++){
+  buttons[i].addEventListener("click", function(){
+    numbers += this.textContent
+    document.querySelector("h1").textContent = numbers
+  })
+}
+
+buttons[buttons.length - 2].addEventListener("click", function(){
+  if (numbers !== ""){
+    document.querySelector("h1").textContent = math.eval(numbers);
+    numbers = math.eval(numbers);
+  }
+})
+
+buttons[buttons.length - 1].addEventListener("click", function(){
+  document.querySelector("h1").textContent = "0";
+  numbers = "";
+})
